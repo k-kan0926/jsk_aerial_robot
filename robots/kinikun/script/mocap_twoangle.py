f81331cc5512aab436eaa05bc5de7fefb839185c
k-kan0926 <165634937+k-kan0926@users.noreply.github.com>
2024-12-03 20:18:41 +0900

    [kinikun]1203_twoarm_experiment

--- robots/kinikun/script/mocap_twoangle.py
+++ robots/kinikun/script/mocap_twoangle.py
@@ -9,6 +9,7 @@ class Angular_Diff_Publisher:
         self.arm1_pose_subscriber = rospy.Subscriber('arm1/ground_pose', Pose2D, self.store_arm1_angular)
         self.arm2_pose_subscriber = rospy.Subscriber('arm2/ground_pose', Pose2D, self.store_arm2_angular)        
         self.body_pose_subscriber = rospy.Subscriber('body/ground_pose', Pose2D, self.store_body_angular)
+
         # JointStateのパブリッシャーに変更
         self.angular_diff_publisher = rospy.Publisher('/quadrotor1/joint_states', JointState, queue_size=1)
         # self.angular_diff_publisher = rospy.Publisher('/quadrotor1/joints_ctrl', JointState, queue_size=1)
@@ -32,24 +33,13 @@ class Angular_Diff_Publisher:
             angular_diff2 = self.arm2_angular - self.body_angular
 
             # JointStateメッセージを作成して角度差をpositionに設定
-            joint_state_msg1 = JointState()
-            joint_state_msg1.name = ['arm3_joint']
-            joint_state_msg1.position = [angular_diff1] 
-            joint_state_msg1.header.stamp = rospy.Time.now()
-            joint_state_msg1.velocity = [0.0]  
-            joint_state_msg1.effort = [0.0]  
-            
-            joint_state_msg2 = JointState()
-            joint_state_msg2.name = ['arm4_joint']
-            joint_state_msg2.position = [angular_diff2] 
-            joint_state_msg2.header.stamp = rospy.Time.now()
-            joint_state_msg2.velocity = [0.0]  
-            joint_state_msg2.effort = [0.0]  
-            
+            joint_state_msg = JointState()
+            joint_state_msg.name = ['arm3_joint', 'arm2_joint']
+            joint_state_msg.position = [angular_diff1, angular_diff2]
+            joint_state_msg.header.stamp = rospy.Time.now()
             # パブリッシュ
-            self.angular_diff_publisher.publish(joint_state_msg1)
-            self.angular_diff_publisher.publish(joint_state_msg2)
-            # rospy.loginfo(f"Published angular_diff as angle: {angular_diff} for arm3_joint")
+            self.angular_diff_publisher.publish(joint_state_msg)
+
 
 if __name__ == '__main__':
     rospy.init_node('ang_diff_node')
